a22e7908fdbbdd1dccb8a10c2bf500434b291cd7
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-31 19:27:35 +0200

    added PathVariable and normalized REST

--- src/main/java/com/example/app/controllers/BookController.java
+++ src/main/java/com/example/app/controllers/BookController.java
@@ -1,21 +1,28 @@
 package com.example.app.controllers;
 
 import com.example.app.entities.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class BookController {
 
     @GetMapping("/books")
-    public Book books(){
+    public List<Book> books(){
 
+        var book1 =new Book("Great Expectations","Charles Dickens");
+        var book2 =new Book("Great Expectations","Charles Dickens");
+        var books = Arrays.asList(book1,book2);
+        return books;
+    }
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") Long id){
         var book =new Book("Great Expectations","Charles Dickens");
+        book.setId(id);
         return book;
     }
-
     @PostMapping("/books")
     public Book createBook(@RequestBody Book book) {
         System.out.println("creating :" + book);
